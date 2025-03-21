d5bff64b130413a36b9a6e135fd3ad50524b9f94
Lenn-rao <lenn.rao@student.moringaschool.com>
2025-03-21 16:59:51 +0300

    scoping codes

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+// 1. Declare customerName in global scope
+var customerName = 'bob';
+
+// 2. Function to uppercase customerName
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// 3. Function to set bestCustomer in global scope
+function setBestCustomer() {
+    bestCustomer = 'not bob';  // Implicit global variable
+}
+
+// 4. Function to overwrite bestCustomer
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+// 5. Declare leastFavoriteCustomer as a constant
+const leastFavoriteCustomer = 'someone';
+
+// 6. Function to attempt changing leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'another person';  // This will throw an error
+}
